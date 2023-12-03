7b0800f34b0681783f90a0afcaca6e4016ae6563
Vinícius Soares Victorelli <58866162+vinisvictorelli@users.noreply.github.com>
2023-12-03 16:12:33 -0300

    Delete voice directory

--- voice/app.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-from gtts.tokenizer.pre_processors import abbreviations, end_of_line
-from gtts.tokenizer.tokenizer_cases import tone_marks
-def voice(text):
-    tts = gTTS(text=text,lang="pt-br",slow=False,pre_processor_funcs=[abbreviations,end_of_line],tokenizer_func=[tone_marks])
-    tts.save("output.mp3")
-
--- voice/coquitts.py
+++
@@ -1,8 +0,0 @@
-from TTS.api import TTS
-
-api = TTS("tts_models/por/fairseq/vits")
-api.tts_with_vc_to_file(
-    "Ola, meu nome é alice",
-    speaker_wav="output.wav",
-    file_path="teste.wav"
-)
